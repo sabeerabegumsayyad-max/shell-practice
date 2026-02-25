c9293317bb3dc0ab3cd11804f03c5019bfdf139d
sabeerabegumsayyad-max <sabeerabegumsayyad@gmail.com>
2026-02-25 16:45:53 +0530

    helloworld started

---
+++ 05-arguments.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+echo "enter person1 "
+read person1
+
+echo "enter person2 "
+read person2
+
+echo "$person1 : Hey $person2? Is it you?"
+echo "$person2 : Oh $person1! How are you? Its been a long time."
+echo "$person1 : I am fine, what about you? Yes, we last met during"
+echo "$person2 : I’m good too."
+echo "$person1 : What are you doing now?"
+echo "$person2 : Well, I have started my undergraduates"
