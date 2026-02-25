5c23c23c589011bf7b151f928b9135c0a4b834c8
sabeerabegumsayyad-max <sabeerabegumsayyad@gmail.com>
2026-02-25 16:38:38 +0530

    helloworld started

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+person1=Rita
+person2=Tina
+
+echo "$person1 : Hey $person2? Is it you?"
+echo "$person2 : Oh $person1! How are you? Its been a long time."
+echo "$person1 : I am fine, what about you? Yes, we last met during"
+echo "$person2 : I’m good too."
+echo "$person1 : What are you doing now?"
+echo "$person2 : Well, I have started my undergraduates"
