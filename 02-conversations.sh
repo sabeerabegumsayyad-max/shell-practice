0945bdb55b9143d7a4d7bf0ddc654307acf3899b
sabeerabegumsayyad-max <sabeerabegumsayyad@gmail.com>
2026-02-25 16:24:31 +0530

    helloworld started

---
+++ 02-conversations.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+echo "Rita : Hey Tina? Is it you?"
+echo "Tina : Oh Rita! How are you? Its been a long time."
+echo "Rita : I am fine, what about you? Yes, we last met during"
+echo "Tina : I’m good too."
+echo "Rita : What are you doing now?"
+echo "Tina : Well, I have started my undergraduate"
