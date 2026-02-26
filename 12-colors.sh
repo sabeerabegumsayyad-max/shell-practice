ba418259aa2a25885c50aa1d8a93257ceb16dcd2
sabeerabegumsayyad-max <sabeerabegumsayyad@gmail.com>
2026-02-26 23:10:23 +0530

    helloworld started

---
+++ 12-colors.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+userid=$(id -u)
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+N=\e[0m
+if [ $userid -ne 0 ]
+then
+    echo "please run this script with root access "
+    exit 1
+else
+    echo "running this script with root access "
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ] 
+    then
+        echo -e " $2 install is $G success $N"
+    else
+        echo " $2 install is failure"
+        exit 1
+    fi
+}
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "mysql is not installed ...going to install"
+    dnf install mysql -y
+    VALIDATE $? "mysql"
+else
+    echo -e " $G mysql already installed nothing to do $N"
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then
+    echo "python3 is not installed ...going to install"
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo -e " $G python3 already installed nothing to do $N"
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+    echo "nginx is not installed ...going to install"
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo -e "$G nginx already installed nothing to do $N"
+    exit 1
+fi
