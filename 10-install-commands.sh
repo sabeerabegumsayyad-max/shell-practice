d67640994906e2ae24332ff6b3b2b1bc900c39da
sabeerabegumsayyad-max <sabeerabegumsayyad@gmail.com>
2026-02-26 19:14:35 +0530

    helloworld started

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -5,8 +5,18 @@ userid=$(id -u)
 if [ $userid -ne 0 ]
 then
     echo "please run this script with root access "
+    exit 1
 else
     echo "running this script with root access "
 fi
 
 dnf install mysql -y
+
+if [ $? -eq 0 ] 
+then
+    echo " mysql install is success"
+else
+    echo " mysql install is failure"
+    exit 1
+fi
+
