3f45dc92719442605308c65c086b645c1d7f9cd3
sabeerabegumsayyad-max <sabeerabegumsayyad@gmail.com>
2026-02-26 21:23:34 +0530

    helloworld started

---
+++ 11-functions.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]
+then
+    echo "please run this script with root access "
+    exit 1
+else
+    echo "running this script with root access "
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ] 
+    then
+        echo " $2 install is success"
+    else
+        echo " $2 install is failure"
+        exit 1
+    fi
+}
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "mysql is not installed ...going to install"
+    dnf install mysql -y
+    VALIDATE $? "mysql"
+else
+    echo "mysql already installed nothing to do"
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then
+    echo "python3 is not installed ...going to install"
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo "python3 already installed nothing to do"
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+    echo "nginx is not installed ...going to install"
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo "nginx already installed nothing to do"
+    exit 1
+fi
