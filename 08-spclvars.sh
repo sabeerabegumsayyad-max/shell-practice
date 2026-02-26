7c3eafb16e84fdc42956f8305b9188e266fcecf2
sabeerabegumsayyad-max <sabeerabegumsayyad@gmail.com>
2026-02-26 17:35:46 +0530

    helloworld started

---
+++ 08-spclvars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "to get values $@"
+echo "no of variables $#"
+echo "scriptname : $0"
+echo "current directory : $pwd "
+echo " user name : $user "
+echo " home dir : $home"
+echo " PiD of current script : $$ "
+echo " PID of back bgound ID : $! "
