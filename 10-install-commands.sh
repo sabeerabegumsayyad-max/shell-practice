4d815aa1ba97950dec225d04b5fa107e37c14303
sabeerabegumsayyad-max <sabeerabegumsayyad@gmail.com>
2026-02-26 18:11:58 +0530

    helloworld started

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -2,9 +2,10 @@
 
 userid=$(id -u)
 
-if [$userid -ne 0]
+if [ $userid -ne 0 ]
 then
     echo "please run this script with root access "
+    exit 1
 else
     echo "running this script with root access "
 fi
