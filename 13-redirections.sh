4dc87a54c1fbcb1a67119c95e0fd218ff8c07ed2
sabeerabegumsayyad-max <sabeerabegumsayyad@gmail.com>
2026-02-26 23:47:33 +0530

    helloworld started

--- 13-redirections.sh
+++ 13-redirections.sh
@@ -12,7 +12,7 @@ then
     echo "please run this script with root access ">>$log_file
     exit 1
 else
-    echo "running this script with root access "| tee -a $log_file
+    echo "running this script with root access " | tee -a $log_file
 fi
 
 VALIDATE(){
@@ -29,30 +29,30 @@ dnf list installed mysql>>$log_file
 
 if [ $? -ne 0 ]
 then
-    echo "mysql is not installed ...going to install"| tee -a $log_file
+    echo "mysql is not installed ...going to install" | tee -a $log_file
     dnf install mysql -y>>$log_file
     VALIDATE $? "mysql"
 else
-    echo "mysql already installed nothing to do">>| tee -a $log_file
+    echo "mysql already installed nothing to do">> | tee -a $log_file
 fi
 
 dnf list installed python3>>$log_file
 if [ $? -ne 0 ]
 then
-    echo "python3 is not installed ...going to install"| tee -a $log_file
+    echo "python3 is not installed ...going to install" | tee -a $log_file
     dnf install python3 -y>>$log_file
     VALIDATE $? "python3"
 else
-    echo "python3 already installed nothing to do"| tee -a $log_file
+    echo "python3 already installed nothing to do" | tee -a $log_file
 fi
 
 dnf list installed nginx>>$log_file
 if [ $? -ne 0 ]
 then
-    echo "nginx is not installed ...going to install"| tee -a $log_file
+    echo "nginx is not installed ...going to install" | tee -a $log_file
     dnf install nginx -y>>$log_file
     VALIDATE $? "nginx"
 else
-    echo "nginx already installed nothing to do"| tee -a $log_file
+    echo "nginx already installed nothing to do" | tee -a $log_file
     exit 1
 fi
